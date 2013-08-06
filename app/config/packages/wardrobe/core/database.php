541e40da0f324a021f8eacf0564652767d208c3e
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 15:48:41 -0400

    Init app skeleton

---
+++ app/config/packages/wardrobe/core/database.php
@@ -0,0 +1,46 @@
+<?php
+
+return array(
+	'default' => 'default',
+
+	'connections' => array(
+
+		'sqlite' => array(
+			'driver'   => 'sqlite',
+			'database' => app_path().'/database/wardrobe.sqlite',
+			'prefix'   => '',
+		),
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'wardrobe',
+			'username'  => 'root',
+			'password'  => 'root',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+		'pgsql' => array(
+			'driver'   => 'pgsql',
+			'host'     => 'localhost',
+			'database' => 'database',
+			'username' => 'root',
+			'password' => '',
+			'charset'  => 'utf8',
+			'prefix'   => '',
+			'schema'   => 'public',
+		),
+
+		'sqlsrv' => array(
+			'driver'   => 'sqlsrv',
+			'host'     => 'localhost',
+			'database' => 'database',
+			'username' => 'root',
+			'password' => '',
+			'prefix'   => '',
+		),
+
+	),
+);
---
+++ app/config/packages/wardrobe/core/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'blog_group_rules' => array(
+		// 'prefix' => 'blog',
+		// 'domain' => 'blog.site.com'
+	),
+
+	'admin_group_rules' => array(
+		'prefix' => 'wardrobe',
+		// 'domain' => 'blog.site.com'
+	),
+
+	'api_group_rules' => array(
+		'prefix' => 'api',
+		// 'domain' => 'blog.site.com'
+	),
+);
