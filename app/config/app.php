7b9b29730ece99db74d29cbdece0dee7a245f958
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 16:23:02 -0400

    Added Wardrobe Alias for simple theme

--- app/config/app.php
+++ app/config/app.php
@@ -180,6 +180,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Wardrobe'        => 'Wardrobe\Core\Facades\WardrobeFacade',
 
 	),
 
