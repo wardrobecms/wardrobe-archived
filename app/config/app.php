251bf2a443a2323d148d98594e12b2e6e9e33946
Eric Barnes <eric@ericlbarnes.com>
2014-01-16 20:25:40 -0500

    Remove clockwork

--- app/config/app.php
+++ app/config/app.php
@@ -107,9 +107,9 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
-		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
-		'Wardrobe\Cabinet\CabinetServiceProvider',
-		'Clockwork\Support\Laravel\ClockworkServiceProvider',
+//		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+//		'Wardrobe\Cabinet\CabinetServiceProvider',
+
 	),
 
 	/*
@@ -175,8 +175,6 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
-		'Clockwork' => 'Clockwork\Support\Laravel\Facade',
 
 	),
 
