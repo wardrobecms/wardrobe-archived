2846249bf5fab78fc89aef76b5dc6980e1e6152f
Eric Barnes <eric@ericlbarnes.com>
2014-06-02 19:23:01 -0400

    Updating for laravel 4.2

--- app/config/app.php
+++ app/config/app.php
@@ -56,6 +56,19 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
+	| Application Fallback Locale
+	|--------------------------------------------------------------------------
+	|
+	| The fallback locale determines the locale to use when the current one
+	| is not available. You may change the value to correspond to any of
+	| the language folders that are provided through your application.
+	|
+	*/
+
+	'fallback_locale' => 'en',
+
+	/*
+	|--------------------------------------------------------------------------
 	| Encryption Key
 	|--------------------------------------------------------------------------
 	|
@@ -66,6 +79,8 @@ return array(
 	*/
 
 	'key' => 'DPtet9EJRGCnK40FPXduWSGOOTFoSCdb',
+
+	'cipher' => MCRYPT_RIJNDAEL_256,
 
 	/*
 	|--------------------------------------------------------------------------
